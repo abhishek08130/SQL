14aaf4450fb39760309b666baea758b891613de5
abhishek08130 <abhishekbhatt08130@gmail.com>
2025-01-12 23:19:49 +0530

    1633 Added

---
+++ leetcode Sol top 50/Leetcode DATABASE Questions/1633.percentage-of-users-attended-a-contest.sql
@@ -0,0 +1,19 @@
+--
+-- @lc app=leetcode id=1633 lang=mysql
+--
+-- [1633] Percentage of Users Attended a Contest
+--
+
+-- @lc code=start
+SELECT 
+    r.contest_id,
+    ROUND(COUNT(DISTINCT r.user_id) / (SELECT COUNT(*) FROM Users) * 100, 2) AS percentage
+FROM 
+    Register r
+GROUP BY 
+    r.contest_id
+ORDER BY 
+    percentage DESC, 
+    r.contest_id ASC;
+-- @lc code=end
+
