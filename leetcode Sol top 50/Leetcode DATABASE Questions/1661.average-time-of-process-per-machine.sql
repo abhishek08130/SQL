32bb71a745068d88d5f0b84016ff2f831a521d41
abhishek08130 <abhishekbhatt08130@gmail.com>
2025-01-13 22:28:14 +0530

    1661 Added

---
+++ leetcode Sol top 50/Leetcode DATABASE Questions/1661.average-time-of-process-per-machine.sql
@@ -0,0 +1,24 @@
+--
+-- @lc app=leetcode id=1661 lang=mysql
+--
+-- [1661] Average Time of Process per Machine
+--
+
+-- @lc code=start
+# Write your MySQL query statement below
+SELECT 
+    machine_id,
+    ROUND(AVG(end_time - start_time), 3) AS processing_time
+FROM (
+    SELECT 
+        machine_id,
+        process_id,
+        MAX(CASE WHEN activity_type = 'start' THEN timestamp END) AS start_time,
+        MAX(CASE WHEN activity_type = 'end' THEN timestamp END) AS end_time
+    FROM Activity
+    GROUP BY machine_id, process_id
+) AS process_times
+GROUP BY machine_id;
+
+-- @lc code=end
+
